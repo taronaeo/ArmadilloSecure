88bc23891db82b6ea1b01bb6c4ea4f56751aa8e9
Aaron Teo <aaron.teo@riv-alumni.com>
2023-12-01 20:01:33 +0800

    feat(wrapper): add tailwindcss + daisyui support

---
+++ apps/wrapper/tailwind.config.cjs
@@ -0,0 +1,19 @@
+const daisyui = require('daisyui');
+const typography = require('@tailwindcss/typography');
+const forms = require('@tailwindcss/forms');
+
+/** @type {import('tailwindcss').Config}*/
+const config = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+
+  theme: {
+    extend: {},
+  },
+
+  plugins: [forms, typography, daisyui],
+  daisyui: {
+    themes: ['dracula'],
+  },
+};
+
+module.exports = config;
