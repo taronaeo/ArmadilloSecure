42e84d4de4a56f403fe94c24a0682d1d605ed4d6
Aaron Teo <aaron.teo@riv-alumni.com>
2023-12-02 00:18:54 +0800

    fix(backend): ensure consistent theme with wrapper

---
+++ apps/backend/src/routes/theme-test/+page.svelte
@@ -0,0 +1,19 @@
+<h2 class="text-3xl">Buttons</h2>
+
+<button class="btn">Button</button>
+<button class="btn btn-neutral">Neutral</button>
+<button class="btn btn-primary">Primary</button>
+<button class="btn btn-secondary">Secondary</button>
+<button class="btn btn-accent">Accent</button>
+<button class="btn btn-ghost">Ghost</button>
+<button class="btn btn-link">Link</button>
+
+<h2 class="text-3xl">Dropdown</h2>
+
+<div class="dropdown">
+	<div tabindex="0" role="button" class="btn m-1">Click</div>
+	<ul tabindex="-1" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+		<li><a href="/">Item 1</a></li>
+		<li><a href="/">Item 2</a></li>
+	</ul>
+</div>
