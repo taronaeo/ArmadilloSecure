1d05702b7afdd234addf3772223d5fbedd9a4c99
Aaron Teo <aaron.teo@riv-alumni.com>
2023-12-02 16:08:10 +0800

    feat(backend): init firebase connection

---
+++ apps/backend/src/lib/firebase/auth.ts
@@ -0,0 +1,45 @@
+import { FirebaseError } from 'firebase/app';
+import {
+	signOut as _signOut,
+	signInWithEmailAndPassword,
+	createUserWithEmailAndPassword
+} from 'firebase/auth';
+
+import { auth } from '$lib/firebase';
+import { authStatusStore } from '$lib/stores';
+
+export function signOut() {
+	return _signOut(auth);
+}
+
+export async function signUpEmailPassword(email: string, password: string) {
+	try {
+		await createUserWithEmailAndPassword(auth, email, password);
+		return authStatusStore.set(null);
+	} catch (error) {
+		if (!(error instanceof FirebaseError)) {
+			throw new Error('Caught non-Firebase error!');
+		}
+
+		return authStatusStore.set({
+			code: error.code,
+			message: error.message
+		});
+	}
+}
+
+export async function signInEmailPassword(email: string, password: string) {
+	try {
+		await signInWithEmailAndPassword(auth, email, password);
+		return authStatusStore.set(null);
+	} catch (error) {
+		if (!(error instanceof FirebaseError)) {
+			throw new Error('Caught non-Firebase error!');
+		}
+
+		return authStatusStore.set({
+			code: error.code,
+			message: error.message
+		});
+	}
+}
--- apps/backend/src/routes/+layout.svelte
+++ apps/backend/src/routes/+layout.svelte
@@ -1,1 +1,18 @@
-<script>import "../app.postcss";</script><slot></slot>
+<script>
+	import '../app.postcss';
+	import { authStore } from '$lib/stores';
+
+	// Preload auth state
+	$authStore;
+</script>
+
+{#if $authStore === undefined}
+	<!-- Auth preload screen -->
+	<div
+		class="z-50 fixed top-0 w-screen h-screen
+          flex flex-col justify-center items-center">
+		<span class="loading loading-infinity loading-lg" />
+	</div>
+{:else}
+	<slot />
+{/if}
