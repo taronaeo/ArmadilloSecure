60cf55b699f6c2d72ed1fd14b48e668ec4254ead
Quekyy <joshuaq1209@gmail.com>
2023-12-08 16:46:34 +0800

    fix(backend): fixed all PR issues

--- apps/backend/src/routes/login/+page.svelte
+++ apps/backend/src/routes/login/+page.svelte
@@ -1,76 +1,76 @@
 <!-- JavaScript code for Continue button -->
 <script lang="ts">
+  // import for forms
+  import { createForm } from "svelte-forms-lib";
+  
 	let confirm: boolean = false;
 </script>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body class="flex items-center justify-center h-screen">
+<div class="flex items-center justify-center h-screen">
 
     <!-- Login Form - Card -->
-    <div class="w-4/12 p-10 border border-primary rounded-md shadow-lg">
+    <div class="w-full max-w-lg p-10 border border-primary rounded-md shadow-lg">
 
         <!-- Login Form - Header -->
         <p class="text-3xl font-semibold text-center mb-5">
             Welcome Back
         </p>
         
-        <!-- Login Form - Email Input -->
-        <div>
-            <label class="form-control w-full max-w-md">
-                <div class="label">
-                    <span class="label-text">Email</span>
-                </div>
-                <input type="text" placeholder="Name@example.com" class="input input-bordered w-full max-w-md 
-                focus:ring-primary focus:border-primary duration-300 focus:outline-none" /> 
-            </label>
-        </div>
+        <!-- Login Form - Form Content -->
+        <form>
+            <!-- Form Content - Email Input -->
+            <div>
+                <label class="form-control w-full max-w-md">
+                    <div class="label">
+                        <span class="label-text">Email</span>
+                    </div>
+                    <input type="text" placeholder="Name@example.com" class="input input-bordered w-full max-w-md 
+                    focus:ring-primary focus:border-primary duration-300 focus:outline-none" /> 
+                </label>
+            </div>
 
-        <!-- Login Form - Password Input -->
-        <div>
-            <label class="form-control w-full max-w-md">
-                <div class="label">
-                    <span class="label-text">Password</span>
-                </div>
-                <input type="text" placeholder="••••••••" class="input input-bordered w-full max-w-md 
-                focus:ring-primary focus:border-primary duration-300 focus:outline-none" /> 
-            </label>
-        </div>
+            <!-- Form Content - Password Input -->
+            <div>
+                <label class="form-control w-full max-w-md">
+                    <div class="label">
+                        <span class="label-text">Password</span>
+                    </div>
+                    <input type="text" placeholder="••••••••" class="input input-bordered w-full max-w-md 
+                    focus:ring-primary focus:border-primary duration-300 focus:outline-none" /> 
+                </label>
+            </div>
 
-        <!-- Login Form - Submit/Continue Button -->
-		{#if !confirm}
-            <button
-                type="submit"
-                class="btn btn-primary w-full max-w-2xl mt-8 mb-2 focus:ring-2 focus:ring-success"
-                on:click={() => {
-                    confirm = true;
-                }}>Continue</button
-            >
-
-        <!-- Login Form - Submit/Continue Button with animation -->
-        {:else}
-            <button class="btn btn-primary w-full mt-5 mb-2 flex flex-col items-center justify-center">
-                <svg
-                    class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
+            <!-- Form Content - Submit/Continue Button -->
+            {#if !confirm}
+                <button
+                    type="submit"
+                    class="btn btn-primary w-full max-w-2xl mt-8 mb-2 focus:ring-2 focus:ring-success"
+                    on:click={() => {
+                        confirm = true;
+                    }}>Continue</button
                 >
-                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"
-                    ></circle>
-                    <path
-                        class="opacity-75"
-                        fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                    ></path>
-                </svg>
-            </button>
-        {/if}
 
-
+            <!-- Form Content - Submit/Continue Button with animation -->
+            {:else}
+                <button class="btn btn-primary w-full mt-5 mb-2 flex flex-col items-center justify-center">
+                    <svg
+                        class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                    >
+                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"
+                        ></circle>
+                        <path
+                            class="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
+                    </svg>
+                </button>
+            {/if}
+        </form>
+    
         <!-- Login Form - Link to Sign up -->
         <p class="text-center">
             Don't have an account? 
@@ -81,4 +81,4 @@
 
     </div>
 
-</body>
+</div>
--- apps/backend/src/routes/signup/+page.svelte
+++ apps/backend/src/routes/signup/+page.svelte
@@ -1,5 +1,8 @@
 <!-- JavaScript code -->
 <script lang="ts">
+  // import for forms
+  import { createForm } from "svelte-forms-lib";
+
 	// JavaScript code for Continue button
 	let confirm: boolean = false;
 
@@ -11,12 +14,6 @@
   	}
 </script>
 
-<head>
-	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<title>Signup</title>
-</head>
-
 <div class="flex items-center justify-center h-screen">
 	<!-- Signup Form - Logo -->
 
@@ -26,94 +23,96 @@
 		<!-- Signup Form - Header -->
 		<p class="text-3xl font-semibold text-center mb-5">Create your account</p>
 
-		<!-- Signup Form - Corporate Email Input -->
-		<div>
-			<label class="form-control w-full max-w-2xl">
-				<div class="label">
-					<span class="label-text">Corporate Email Address</span>
-				</div>
-				<input
-					type="text"
-					placeholder="Name@example.com"
-					class="input input-bordered w-full max-w-2xl
-                focus:ring-primary focus:border-primary duration-300 focus:outline-none"
-				/>
-			</label>
-		</div>
+    <form action="">
+      <!-- Signup Form - Corporate Email Input -->
+      <div>
+        <label class="form-control w-full max-w-2xl">
+          <div class="label">
+            <span class="label-text">Corporate Email Address</span>
+          </div>
+          <input
+            type="text"
+            placeholder="Name@example.com"
+            class="input input-bordered w-full max-w-2xl
+                  focus:ring-primary focus:border-primary duration-300 focus:outline-none"
+          />
+        </label>
+      </div>
 
-		<!-- Signup Form - Password Input -->
-		<div>
-			<label class="form-control w-full max-w-2xl">
-				<div class="label">
-					<span class="label-text">Password</span>
-				</div>
-				<input
-					type="text"
-					placeholder="••••••••"
-					class="input input-bordered w-full max-w-2xl
-                focus:ring-primary focus:border-primary duration-300 focus:outline-none"
-				/>
-			</label>
-		</div>
+      <!-- Signup Form - Password Input -->
+      <div>
+        <label class="form-control w-full max-w-2xl">
+          <div class="label">
+            <span class="label-text">Password</span>
+          </div>
+          <input
+            type="text"
+            placeholder="••••••••"
+            class="input input-bordered w-full max-w-2xl
+                  focus:ring-primary focus:border-primary duration-300 focus:outline-none"
+          />
+        </label>
+      </div>
 
-		<!-- Signup Form - Confirm Password Input -->
-		<div>
-			<label class="form-control w-full max-w-2xl">
-				<div class="label">
-					<span class="label-text">Confirm Password</span>
-				</div>
-				<input
-					type="text"
-					placeholder="••••••••"
-					class="input input-bordered w-full max-w-2xl
-                focus:ring-primary focus:border-primary duration-300 focus:outline-none"
-				/>
-			</label>
-		</div>
+      <!-- Signup Form - Confirm Password Input -->
+      <div>
+        <label class="form-control w-full max-w-2xl">
+          <div class="label">
+            <span class="label-text">Confirm Password</span>
+          </div>
+          <input
+            type="text"
+            placeholder="••••••••"
+            class="input input-bordered w-full max-w-2xl
+                  focus:ring-primary focus:border-primary duration-300 focus:outline-none"
+          />
+        </label>
+      </div>
 
-		<div class="form-control mt-7">
-			<label 
-				class="label cursor-pointer">
-				<input type="checkbox" class="checkbox checkbox-primary mr-5" bind:checked={yes} on:click={toggleCheckbox}/>
-				<span class="label-text">By signing up, you are creating a Armadillo account, and you agree to Armadillo's 
-					<a class="text-primary hover:text-accent duration-300" href="/login"> Terms of Use </a>
-					 and 
-					<a class="text-primary hover:text-accent duration-300" href="/login"> Privacy Policy </a>
-				</span> 
-			</label>
-		</div>
+      <div class="form-control mt-7">
+        <label 
+          class="label cursor-pointer">
+          <input type="checkbox" class="checkbox checkbox-primary mr-5" bind:checked={yes} on:click={toggleCheckbox}/>
+          <span class="label-text">By signing up, you are creating a Armadillo account, and you agree to Armadillo's 
+            <a class="text-primary hover:text-accent duration-300" href="/login"> Terms of Use </a>
+            and 
+            <a class="text-primary hover:text-accent duration-300" href="/login"> Privacy Policy </a>
+          </span> 
+        </label>
+      </div>
 
-		<!-- Signup Form - Submit/Continue Button -->
-		{#if !confirm}
-			<button
-				type="submit"
-				class="btn btn-primary w-full max-w-2xl mt-8 mb-2 focus:ring-2 focus:ring-success"
-				on:click={() => {
-					confirm = true;
-				}}>Create Account</button
-			>
+      <!-- Signup Form - Submit/Continue Button -->
+      {#if !confirm}
+        <button
+          type="submit"
+          class="btn btn-primary w-full max-w-2xl mt-8 mb-2 focus:ring-2 focus:ring-success"
+          on:click={() => {
+            confirm = true;
+          }}>Create Account</button
+        >
 
-			<!-- <button type="submit" id="continueButton" class="btn btn-primary w-80 mt-8 mb-2 focus:ring-2 focus:ring-success" on:click={startLoading}>Continue</button> -->
+        <!-- <button type="submit" id="continueButton" class="btn btn-primary w-80 mt-8 mb-2 focus:ring-2 focus:ring-success" on:click={startLoading}>Continue</button> -->
 
-		<!-- Signup Form - Submit/Continue Button with animation -->
-		{:else}
-			<button class="btn btn-primary w-full mt-5 mb-2 flex flex-col items-center justify-center">
-				<svg
-					class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-				>
-					<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"
-					></circle>
-					<path
-						class="opacity-75"
-						fill="currentColor"
-						d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-					></path>
-				</svg>
-			</button>
-		{/if}
+      <!-- Signup Form - Submit/Continue Button with animation -->
+      {:else}
+        <button class="btn btn-primary w-full mt-5 mb-2 flex flex-col items-center justify-center">
+          <svg
+            class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"
+            ></circle>
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        </button>
+      {/if}
+    </form>
 
 		<!-- Signup Form - Link to Login -->
 		<p class="text-center">
