1631a2adfa7e77dbb9e4b6427e151f439ce7d10a
Aaron Teo <aaron.teo@riv-alumni.com>
2023-12-08 22:03:02 +0800

    feat(functions-gcp): add file classification api

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -1,3 +1,7 @@
 export * from './constants/collections';
 
+export * from './models/FSMetadata';
 export * from './models/UserDocument';
+export * from './models/CFApiWrapperResponse';
+export * from './models/FSFileDocument';
+export * from './models/FSFileClassification';
