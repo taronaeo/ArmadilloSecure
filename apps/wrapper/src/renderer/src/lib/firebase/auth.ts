af30c8b9836cb75754203e4c3edd111e0fdd1b79
Aaron Teo <aaron.teo@riv-alumni.com>
2023-12-13 00:00:42 +0800

    refactor(backend,wrapper): error handling now directly at client-side

--- apps/wrapper/src/renderer/src/lib/firebase/auth.ts
+++ apps/wrapper/src/renderer/src/lib/firebase/auth.ts
@@ -1,24 +1,55 @@
+import { DEV as dev } from 'esm-env';
 import { FirebaseError } from 'firebase/app';
 import { signOut as _signOut, signInWithCustomToken } from 'firebase/auth';
 
 import { auth } from '../firebase';
-import { authStatusStore } from '../stores';
 
-export function signOut() {
-  return _signOut(auth);
+type ErrorCallback = FirebaseError | null;
+
+/**
+ * Signs out the current user.
+ *
+ * @param errorCb   A callback function to be called with any errors encountered during sign-out.
+ *
+ * @callback ErrorCallback
+ * @property code   The error code provided by Firebase.
+ *
+ * @returns Resolves if sign-out is successful, rejects with an error if any occurs.
+ */
+export async function signOut(errorCb: (error: ErrorCallback) => void) {
+  try {
+    errorCb(null);
+    await _signOut(auth);
+  } catch (error) {
+    if (dev) console.error(error);
+    if (!(error instanceof FirebaseError)) throw new Error('Caught unknown error!');
+
+    return errorCb(error);
+  }
 }
 
-export async function signInServerToken(authToken: string) {
+/**
+ * Signs in a user using a server-generated custom token.
+ *
+ * @param authToken The server-generated custom token for user authentication.
+ * @param errorCb   A callback function to be called with any errors encountered during sign-in.
+ *
+ * @callback ErrorCallback
+ * @property code   The error code provided by Firebase.
+ *
+ * @returns Resolves if sign-in is successful, rejects with an error if any occurs.
+ */
+export async function signInServerToken(
+  authToken: string,
+  errorCb: (error: ErrorCallback) => void
+) {
   try {
-    authStatusStore.set(null);
+    errorCb(null);
     await signInWithCustomToken(auth, authToken);
-    return authStatusStore.set(null);
   } catch (error) {
+    if (dev) console.error(error);
     if (!(error instanceof FirebaseError)) throw new Error('Caught non-firebase error!');
 
-    return authStatusStore.set({
-      code: error.code,
-      message: error.message,
-    });
+    return errorCb(error);
   }
 }
