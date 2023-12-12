6085e451df664f5ceef064c2f0be22f9996ad293
Aaron Teo <aaron.teo@riv-alumni.com>
2023-12-12 19:01:23 +0800

    refactor(wrapper): refine firebase api for wrapper

--- apps/wrapper/src/renderer/src/lib/firebase/functions.ts
+++ apps/wrapper/src/renderer/src/lib/firebase/functions.ts
@@ -1,21 +1,29 @@
-// import { dev } from '$app/environment';
 import { functions } from '../firebase';
 
 import { httpsCallable } from 'firebase/functions';
 
-const endpoints = {
-  // TODO: Populate all Cloud Functions endpoints
+const HTTPS_ENDPOINTS = {
+  http_onRequest_fileClassification: 'DEPRECATING_SOON',
 };
 
-export const getHttpsCallable = (endpoint: keyof typeof endpoints) =>
+const HTTPS_CALLABLES = {
+  getAuthLoginToken: 'NOT_DEPLOYED',
+  onCall_getFilePassword: 'DEPLOYED',
+  onCall_getFileClassification: 'DEPLOYED',
+};
+
+export const getHttpsCallable = (endpoint: keyof typeof HTTPS_CALLABLES) =>
   httpsCallable(functions, endpoint, {
     timeout: 60000,
   });
 
-export const getHttpsEndpoint = (endpoint: keyof typeof endpoints, region = 'asia-southeast1') => {
+export const getHttpsEndpoint = (
+  endpoint: keyof typeof HTTPS_ENDPOINTS,
+  region = 'asia-southeast1'
+) => {
   if (typeof endpoint === 'undefined' || !endpoint) throw new Error('Missing `endpoint parameter`');
 
-  // TODO: Re-enable when emulators are setup
-  // if (dev) return `http://127.0.0.1:5000/it2566-armadillo/${region}/${endpoint}`;
+  const dev = import.meta.env.DEV;
+  if (dev) return `http://127.0.0.1:5000/it2566-armadillo/${region}/${endpoint}`;
   return `https://${region}-it2566-armadillo.cloudfunctions.net/${endpoint}`;
 };
