3b368a6df93b839b881aed08f603c39b64a429a5
Aaron Teo <aaron.teo@riv-alumni.com>
2023-12-29 18:44:38 +0800

    fix(wrapper): fix random highlighting

--- apps/wrapper/src/preload/index.d.ts
+++ apps/wrapper/src/preload/index.d.ts
@@ -3,23 +3,23 @@ import { ElectronAPI } from '@electron-toolkit/preload';
 declare global {
   interface Window {
     electron: ElectronAPI;
-    api: unknown;
+    api: IpcRequest;
+  }
+
+  interface IpcRequest {
+    getAppName: () => IpcResponse;
+    getFileClass: (_, fileId: string) => Promise<void>;
+    checkFileClass: () => Promise<IpcResponse>;
+    secretChecks: () => Promise<IpcResponse>;
+    ping: () => Promise<void>;
+    checkPing: () => Promise<IpcResponse>;
+    checkCompromisation: () => Promise<IpcResponse>;
+    hasDefaultProgram: () => IpcResponse;
+    launchFile: () => Promise<void>;
+  }
+
+  interface IpcResponse {
+    code: number;
+    message: string;
   }
 }
-
-// interface IpcRequest {
-//   getAppName: () => IpcResponse;
-//   getFileClass: (_, fileId: string) => Promise<void>;
-//   checkFileClass: () => Promise<IpcResponse>;
-//   secretChecks: () => Promise<IpcResponse>;
-//   ping: () => Promise<void>;
-//   checkPing: () => Promise<IpcResponse>;
-//   checkCompromisation: () => Promise<IpcResponse>;
-//   hasDefaultProgram: () => IpcResponse;
-//   launchFile: () => Promise<void>;
-// }
-
-// interface IpcResponse {
-//   code: number;
-//   message: string;
-// }
